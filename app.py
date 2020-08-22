42e580ac37f6f118853c449da26b14ad8eddc3ff
Jeremy Tandjung <jeremytandjung@icloud.com>
2020-08-21 20:01:19 -0700

    done

--- app.py
+++ app.py
@@ -1,2 +1,60 @@
-from pymongo import MongoClient
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_pymongo import PyMongo
 
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'myDatabase'
+app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
+mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+    return "Hello There"
+
+
+@app.route('/star', methods=['GET'])
+def get_all_stars():
+    starDb = mongo.db.stars
+    output = []
+    for s in starDb.find():
+        output.append({'name' : s['name'], 'distance' : s['distance']})
+    return jsonify({'result' : output})
+
+@app.route('/star/', methods=['GET'])
+def get_one_star(name):
+    star = mongo.db.stars
+    s = star.find_one({'name' : name})
+    if s:
+        output = {'name' : s['name'], 'distance' : s['distance']}
+    else:
+        output = "No such name"
+    return jsonify({'result' : output})
+
+
+@app.route('/star', methods=['POST'])
+def add_star():
+    '''
+
+    '''
+    star = mongo.db.stars
+    name = request.json["name"]
+    distance = request.json['distance']
+    star_id = star.insert({'name': name, 'distance': distance})
+    new_star = star.find_one({'_id': star_id })
+    output = {'name' : new_star['name'], 'distance' : new_star['distance']}
+    return jsonify({'result' : output})
+
+@app.errorhandler(404)
+def page_not_found(e):
+    message = {
+        "err" : {
+            "msg" : "Please refer to API docs!"
+        }
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
